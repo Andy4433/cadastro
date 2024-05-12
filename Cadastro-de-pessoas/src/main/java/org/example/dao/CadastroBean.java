7745521ba0d1b36696ce914ffb54875fa78eef8b
Andys4433 <anderson.s.brito613@gmail.com>
2024-05-12 12:49:12 -0300

    teste

--- Cadastro-de-pessoas/src/main/java/org/example/dao/CadastroBean.java
+++ Cadastro-de-pessoas/src/main/java/org/example/dao/CadastroBean.java
@@ -3,19 +3,26 @@ package org.example.dao;
 import org.example.HelloBean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 @ManagedBean(name = "CadastroBean")
 @SessionScoped
 public class CadastroBean {
-    @Inject
+    @ManagedProperty(value="#{HelloBean}")
     private HelloBean helloBean;
+
+    public HelloBean getHelloBean() {
+        return helloBean;
+    }
+
+    public void setHelloBean(HelloBean helloBean) {
+        this.helloBean = helloBean;
+    }
 
     public String cadastrar() {
         try {
@@ -24,14 +31,34 @@ public class CadastroBean {
 
             // Faça o que precisar com o HelloBean
             System.out.println("Dados do HelloBean:");
-            System.out.println("Nome: " + helloBean.getName());
-            System.out.println("Gênero: " + helloBean.getGender());
-            System.out.println("Idade: " + helloBean.getAge());
-            System.out.println("CEP: " + helloBean.getCep());
-            System.out.println("Número: " + helloBean.getNum());
-            System.out.println("Rua: " + helloBean.getRua());
-            System.out.println("Estado: " + helloBean.getEstado());
-            System.out.println("Cidade: " + helloBean.getCidade());
+            String sqlUsuario = "INSERT INTO tabela_usuario (nome, idade, genero) VALUES (?, ?, ?)";
+            PreparedStatement stmtUsuario = conexao.prepareStatement(sqlUsuario);
+            stmtUsuario.setString(1, helloBean.getName());
+            stmtUsuario.setInt(2, helloBean.getAge());
+            stmtUsuario.setString(3, helloBean.getGender());
+            stmtUsuario.executeUpdate();
+            stmtUsuario.close();
+
+            // Obter o id gerado para a tabela_usuario
+            int idUsuario = 0;
+            PreparedStatement stmtIdUsuario = conexao.prepareStatement("SELECT LASTVAL()");
+            ResultSet rsIdUsuario = stmtIdUsuario.executeQuery();
+            if (rsIdUsuario.next()) {
+                idUsuario = rsIdUsuario.getInt(1);
+            }
+            rsIdUsuario.close();
+            stmtIdUsuario.close();
+
+            // Inserir dados na tabela_endereco
+            String sqlEndereco = "INSERT INTO tabela_endereco (id_usuario, cep, rua, estado, cidade) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement stmtEndereco = conexao.prepareStatement(sqlEndereco);
+            stmtEndereco.setInt(1, idUsuario);
+            stmtEndereco.setString(2, helloBean.getCep());
+            stmtEndereco.setString(3, helloBean.getRua());
+            stmtEndereco.setString(4, helloBean.getEstado());
+            stmtEndereco.setString(5, helloBean.getCidade());
+            stmtEndereco.executeUpdate();
+            stmtEndereco.close();
 
             conexao.close();
 
