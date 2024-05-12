a7cdcf9b802bb710e797a75bdd63ce2994bd3272
Andys4433 <anderson.s.brito613@gmail.com>
2024-05-12 14:21:59 -0300

    teste

--- Cadastro-de-pessoas/src/main/java/org/example/dao/CadastroBean.java
+++ Cadastro-de-pessoas/src/main/java/org/example/dao/CadastroBean.java
@@ -8,12 +8,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import org.example.dao.ConexaoPostgreSQL;
 import org.example.HelloBean;
 
 @ManagedBean(name = "CadastroBean")
+@RequestScoped
 public class CadastroBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -28,8 +30,13 @@ public class CadastroBean implements Serializable {
 
             // Faça o que precisar com o HelloBean
             System.out.println("Dados do HelloBean:");
-            String sqlUsuario = "INSERT INTO tabela_usuario (nome, idade, genero) VALUES (?, ?, ?)";
+            System.out.println("Verificando tabela ...");
+            String sqlTabelaUsuario = "CREATE TABLE IF NOT EXISTS tabela_usuario (id SERIAL PRIMARY KEY, nome VARCHAR(100), idade INTEGER, genero VARCHAR(1))";
+            System.out.println("Tabela encontrada !!! ");
+            PreparedStatement stmtTabelaUsuario = conexao.prepareStatement(sqlTabelaUsuario);
+            String sqlUsuario = "INSERT INTO tabela_usuario (id, nome, idade, genero) VALUES (?,?, ?, ?)";
             PreparedStatement stmtUsuario = conexao.prepareStatement(sqlUsuario);
+            stmtUsuario.setString(1, helloBean.getId());
             stmtUsuario.setString(1, helloBean.getName());
             stmtUsuario.setInt(2, helloBean.getAge());
             stmtUsuario.setString(3, helloBean.getGender());
@@ -47,6 +54,8 @@ public class CadastroBean implements Serializable {
             stmtIdUsuario.close();
 
             // Inserir dados na tabela_endereco
+            String sqlTabelaEndereco = "CREATE TABLE IF NOT EXISTS tabela_endereco (id SERIAL PRIMARY KEY, id_usuario INTEGER REFERENCES tabela_usuario(id), cep VARCHAR(10), rua VARCHAR(100), estado VARCHAR(2), cidade VARCHAR(100))";
+            PreparedStatement stmtTabelaEndereco = conexao.prepareStatement(sqlTabelaEndereco);
             String sqlEndereco = "INSERT INTO tabela_endereco (id_usuario, cep, rua, estado, cidade) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement stmtEndereco = conexao.prepareStatement(sqlEndereco);
             stmtEndereco.setInt(1, idUsuario);
